1decea2d2224ca2b48d0550c203d5932f8749ac1
callumleaper <cleaper@calrom.com>
2021-02-05 12:51:23 +0000

    GetMessage API Functionaility

---
+++ MessageBoardAPI/MessageBoardAPI/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBoardAPI.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string MessageText { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ MessageBoardAPI/MessageBoardAPI/Services/MessageService.cs
@@ -0,0 +1,38 @@
+﻿using MessageBoardAPI.Models;
+using MessageBoardAPI.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBoardAPI.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly List<Message> messages = new List<Message>()
+        {
+            new Message { Id = Guid.NewGuid(), Name = "User 1", MessageText = "Test Message 1", CreatedDate = DateTime.Now},
+            new Message { Id = Guid.NewGuid(), Name = "User 1", MessageText = "Test Message 2", CreatedDate = DateTime.Now},
+            new Message { Id = Guid.NewGuid(), Name = "User 2", MessageText = "Test Message 3", CreatedDate = DateTime.Now}
+        };
+
+        public IEnumerable<Message> GetAllMessages()
+        {
+            return messages;
+        }
+
+        public IEnumerable<Message> GetUserMessages(string username)
+        {
+            List<Message> userMessages = new List<Message>();
+            foreach (var item in messages)
+            {
+                if (item.Name.Contains(username))
+                {
+                    userMessages.Add(item);
+                }
+            }
+            return userMessages;
+        }
+    }
+}
